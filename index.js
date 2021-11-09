44d42b03f0d2c322c4b63451f400dab16817b453
Enyelber <enyelber20@gmail.com>
2021-11-09 17:08:30 -0400

    TASK-1 Express Server Configuring the express server on port 3001 and route GET.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+const { reverseText } = require('./utils/custom')
 
 const express = require('express')
 const app = express()
@@ -10,6 +11,26 @@ app.use(express.json())
 // ? Routes
 app.get('/', (request, response) => {
   response.send('API TOOLBOX!')
+})
+
+// ? Enviar un Texto y Recibirlo al reves
+app.get('/iecho', (request, response) => {
+  const text = request.query.text
+
+  if (text) {
+    resp = {
+      codigo: 200,
+      text: reverseText(text),
+    }
+    return response.json(resp)
+  } else {
+    resp = {
+      codigo: 400,
+      error: 'no text',
+    }
+    return response.json(resp)
+    //response.status(400).end()
+  }
 })
 
 const PORT = process.env.PORT || 3001
---
+++ utils/custom.js
@@ -0,0 +1,5 @@
+const reverseText = (str) => {
+  return str.split('').reverse().join('')
+}
+
+module.exports = { reverseText }
